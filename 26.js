b3963e71e6e101a50591529af1c3f4ee4b2dcc6d
团团 <wenyunyan@tuantuandeMacBook-Air.local>
2022-05-09 17:31:51 +0800

    26

---
+++ 26.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  let index = 1;
+  while(index < nums.length) {
+      if (nums[index] === nums[index - 1]) {
+          nums.splice(index, 1);
+      } else {
+          index ++;
+      }
+  }
+  return nums.length;
+};
